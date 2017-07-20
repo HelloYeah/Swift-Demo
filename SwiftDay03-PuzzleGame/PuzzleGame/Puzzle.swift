d445d658e024e69c5b01b40089f7f8334c0c76c2
HelloYeah <648889716@qq.com>
2017-07-20 15:28:25 +0800

    demo03-拼图游戏

---
+++ SwiftDay03-PuzzleGame/PuzzleGame/Puzzle.swift
@@ -0,0 +1,44 @@
+//
+//  Puzzle.swift
+//  PuzzleGame
+//
+//  Created by Sekorm on 2017/3/9.
+//  Copyright © 2017年 YL. All rights reserved.
+//
+
+import UIKit
+
+enum Direction {
+    case DirectionUp
+    case DirectionDown
+    case DirectionLeft
+    case DirectionRight
+}
+
+class Puzzle: NSObject {
+    
+    var  col : NSInteger!
+    var  row : NSInteger!
+    private(set) var  image : UIImage!
+    
+    init(image: UIImage, withCol col: NSInteger, withRow row: NSInteger){
+        super.init()
+        self.col = col
+        self.row = row
+        self.image = image
+    }
+    
+    func move(direction : Direction) {
+        
+        switch direction {
+        case .DirectionUp:
+            row = row - 1
+        case .DirectionDown:
+            row = row + 1
+        case .DirectionLeft:
+            col = col - 1
+        case .DirectionRight:
+            col = col + 1
+        }
+    }
+}
